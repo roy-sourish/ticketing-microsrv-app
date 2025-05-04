21f080bf0d0dc170660629f77e70cdfdbaea1fed
Sourish Roy <sourish.roy1973@gmail.com>
2025-05-04 17:55:12 +0530

    Project Setup

---
+++ auth/src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Auth Service
+ */
+
+import express from "express";
+import { json } from "body-parser";
+
+const app = express();
+app.use(json());
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+  console.log("Auth Service - listening on port 3000!!");
+});
