c7ec59fa5f138c532b160a87d2bd5f89529d7337
Sourish Roy <sourish.roy1973@gmail.com>
2025-06-03 19:18:12 +0530

    fix signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,28 +1,32 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import express, { RequestHandler } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
+const handleSignup: RequestHandler = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).send(errors.array());
+    return;
+  }
+
+  const { email, password } = req.body;
+
+  console.log("Creating a user...");
+
+  res.send({});
+};
+
 router.post(
-  '/api/users/signup',
+  "/api/users/signup",
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
-    body('password')
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters')
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
-    const { email, password } = req.body;
-
-    if (!email || typeof email !== 'string') {
-      res.status(400).send('Provide a valid email');
-    }
-
-    // new User({ email, password })
-  }
+  handleSignup
 );
 
 export { router as signupRouter };
