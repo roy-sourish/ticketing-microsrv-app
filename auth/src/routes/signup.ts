f6a15599ad916ae616d4ef6922d86993e17f01e1
Sourish Roy <sourish.roy1973@gmail.com>
2025-06-11 18:03:27 +0530

    auth service - error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,19 +1,20 @@
 import express, { RequestHandler } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
 const handleSignup: RequestHandler = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.status(400).send(errors.array());
-    return;
+    throw new RequestValidationError(errors.array());
   }
 
   const { email, password } = req.body;
 
   console.log("Creating a user...");
-
+  throw new DatabaseConnectionError();
   res.send({});
 };
 
